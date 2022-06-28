700fefd2ac690b198f3885161d0943efd447c728
lky8967 <lky8967@gmail.com>
2022-06-28 16:47:55 +0900

    도커 테스트중

---
+++ jenkins/src/main/java/com/example/jenkins/controller/DockerTest.java
@@ -0,0 +1,15 @@
+package com.example.jenkins.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerTest {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello 씨발";
+    }
+
+}
