666b44ab55bcdd16b16997a3af2781176702beae
lky8967 <lky8967@gmail.com>
2022-06-30 22:19:31 +0900

    도커 테스트 v2

--- jenkins/src/main/java/com/example/jenkins/JenkinsApplication.java
+++ jenkins/src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -2,10 +2,17 @@ package com.example.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JenkinsApplication {
 
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello spring Boot!";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
     }
